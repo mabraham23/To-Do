259c6945127c277eb486f4cb9e80031f6489ccd7
Mark Abraham <markabraham104@gmail.com>
2019-06-21 10:32:43 -0600

    Fixed bugs

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
-const express = require( "express");
+const express = require( "express" );
 const cors = require( "cors" );
 const uuid = require( "uuid" );
 
 var server = express( );
 var port = process.env.PORT || 3000;
 
-//Data
+// Middleware
+server.use( cors( ) );
+server.use( express.json( ) );
+server.use( express.urlencoded( { extended: false } ) );
+server.use( function ( req, res, next ) {
+    console.log( `New request: ${ req.method } ${ req.path } on ${ new Date( ) }` );
+    next( );
+});
+
+// Data
 var data = require( "./data.js" );
 
-//Endpoints
-server.get( "/todos", function (req, res) {
+// Endpoints
+server.get( "/todos", function ( req, res ) {
     var response = {
         todos: data.todos
     };
     res.json( response );
 });
 
-//Middleware
-server.use( cors( ) );
-server.use( express.json( ) );
-server.use( express.urlencoded( {extended: false} ) );
-server.use( function ( req, res, next ) {
-    console.log( `New request: ${ req.method } ${ req.path } on ${ new Date( ) }` );
-    next( );
-});
-
-//REST endpoints
-
+// Start the server
 server.listen( port, function ( ) {
-    console.log(`Listening on port ${port}`);
+    console.log( `Listening on port ${ port }` );
 });
