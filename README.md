d61ad0a698ca8ce886f54499bf8e4ffb3108aff7
Mark Abraham <markabraham104@gmail.com>
2019-06-21 12:17:11 -0600

    Put //End request

--- README.md
+++ README.md
@@ -4,3 +4,28 @@
 * List: Get /todos
 * Create: POST /todos
 * Update: PUT /todos
+
+
+## - Response
+Status: 200
+Content-type: application/json
+Body: { todoos: todos }
+
+# Create: POST /todos
+## - Request
+Method: POST
+Path: /todos
+Content-type: application/json
+Body: { name: "New todo name" }
+
+## - Response
+### If they did not send over a name field:
+Status: 400
+Content-type: application/json
+Body: {msg: "Message" }
+
+### Else: add the tod to the list and send response
+Status: 201
+Content-type: none
+Body: none 
+
--- server.js
+++ server.js
@@ -25,6 +25,93 @@ server.get( "/todos", function ( req, res ) {
     res.json( response );
 });
 
+//Post /todos
+server.post( "/todos", function ( req, res) {
+    if ( req.body.name == undefined ) {
+        // User did not send a name for their to-do
+        var response = {
+            msg: "Please enter a task name"
+        };
+        res.status( 400 );
+        res.json( response );
+    } else if ( req.body.name == "" ) {
+        // The name was empty
+        var response = {
+            msg: "Please enter a name for the task!"
+        };
+        res.status( 400 );
+        res.json( response );
+    } else {
+        // Add the new todo to the list of todos
+        var new_todo = {
+            id: uuid.v4( ),
+            name: req.body.name,
+            completed: false,
+            editing: false,
+            created_on: new Date (),
+        };
+        data.todos.push( new_todo )
+        res.status( 201 );
+        res.json( new_todo );
+
+    }
+});
+
+//PUT /todos/id
+server.put( "/todos/:id", function ( req, res ) {
+    //console.log( req.params.id )
+
+    //Check for bad request
+    var bad_request = false;
+
+    if ( req.body.name == undefined && req.body.completed == undefined ) {
+        bad_request = true;
+        }
+    if ( req.body.name != undefined && req.body.name == "" ) {
+        bad_request = true;
+        }
+    if ( req.body.completed != undefined && typeof( req.body.completed ) != "boolean") {
+        bad_request = true;
+    }
+
+    if ( bad_request ) {
+        var response = {
+            msg: "Please send over a name or a completed value to update, make sure they are valid as well"
+        }
+        res.status( 400 );
+        res.json( response );
+    } else {
+        //Request is good but make sure the id exists
+        var found = data.todos.some( function ( todo ) {
+            return todo.id == req.params.id;
+        });
+
+        if ( !found ) {
+            var response = {
+                msg: `We could not find a todo with the id of ${ req.params.id }`
+            }
+            res.status( 404 );
+            res.json( response );
+        } else {
+            //We have a good todo to update, now lets update it
+            data.todos.forEach( function ( todo ) {
+                if ( todo.id == req.params.id ) {
+                    if ( req.body.name != undefined ) {
+                        todo.name = req.body.name;
+                    }
+                    if ( req.body.completed != undefined ) {
+                        todo.completed = req.body.completed;
+                    }
+                }
+            });
+            res.status( 204 );
+            res.send( );
+
+        }
+    }
+
+});
+
 // Start the server
 server.listen( port, function ( ) {
     console.log( `Listening on port ${ port }` );
